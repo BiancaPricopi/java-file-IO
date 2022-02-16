adbd0963a49ab43661655cb843854c635b0396cf
Pricopi Bianca <75115098+BiancaPricopi@users.noreply.github.com>
2022-02-16 23:05:22 +0200

    created output.txt

--- LearningFiles/src/LearningFiles/Main.java
+++ LearningFiles/src/LearningFiles/Main.java
@@ -1,9 +1,20 @@
 package LearningFiles;
+import java.io.*;
 
 public class Main 
 {
 	public static void main(String[] args) 
 	{
+		try
+		{
+			BufferedWriter bw = new BufferedWriter(
+					new FileWriter("output.txt"));
+		
+		}
+		catch(Exception error)
+		{
+			return;
+		}
 
 	}
 
