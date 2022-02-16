b4d8663c4182aee3f41f173c31a7bf3ed1109db1
Pricopi Bianca <75115098+BiancaPricopi@users.noreply.github.com>
2022-02-16 23:13:41 +0200

    read from output.txt

--- LearningFiles/src/LearningFiles/Main.java
+++ LearningFiles/src/LearningFiles/Main.java
@@ -12,13 +12,25 @@ public class Main
 			bw.write("Salut\n");
 			bw.write("Asta e alta linie.");
 			bw.close();
-		
 		}
 		catch(Exception error)
 		{
 			return;
 		}
-
+		try
+		{
+			BufferedReader br = new BufferedReader(
+					new FileReader("output.txt"));
+			String row;
+			while((row = br.readLine()) != null)
+			{
+				System.out.println(row);
+			}
+			br.close();
+		}
+		catch(Exception error)
+		{
+			return;
+		}
 	}
-
 }
